60bca909d86347b7c51ec52f80c770a05a68404e
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-04-16 09:22:43 +0430

    Add script to sync js file

--- views/home.php
+++ views/home.php
@@ -21,9 +21,8 @@
         </p>
 	<section>
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" 
-		integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" 
-		crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="./public/js/index.js"></script>
 </body>
 
 </html>
