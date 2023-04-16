41abc4dedc4c0e62360214a9870a7bfbd58ec80c
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-04-16 13:19:47 +0430

    Add goods and rates view fiels

---
+++ views/Rate/show.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="This is a simple CMS for tracing goods based on thier serail or part number.">
+    <meta name="author" content="Mahdi Rezaei">
+    <link rel="shortcut icon" href="./public/img/YadakShop.png">
+    <title>Yadak Shop</title>
+    <link rel="stylesheet" href="./public/css/styles.css">
+</head>
+
+<body>
+<main class="login-page">
+  <section class="form">
+    Show
+  </section>
+</main>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="./public/js/index.js"></script>
+</body>
+
+</html>
