c4b823147bf3d952bd1fc7ba6471ce416d2d5414
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-04-16 13:13:25 +0430

    Add project routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Symfony\Component\Routing\RouteCollection;
 
 // Routes system
 $routes = new RouteCollection();
-$routes->add('login', new Route(constant('URL_SUBFOLDER') . '/', array('controller' => 'LoginController', 'method'=>'indexAction'), array()));
-$routes->add('homepage', new Route(constant('URL_SUBFOLDER') . '/search', array('controller' => 'PageController', 'method'=>'indexAction'), array()));
+$routes->add('login', new Route(constant('URL_SUBFOLDER') . '/', array('controller' => 'LoginController', 'method'=>'index'), array()));
+$routes->add('homepage', new Route(constant('URL_SUBFOLDER') . '/search', array('controller' => 'PageController', 'method'=>'index'), array()));
+$routes->add('goods', new Route(constant('URL_SUBFOLDER') . '/goods', array('controller' => 'GoodController', 'method'=>'index'), array()));
+$routes->add('goods', new Route(constant('URL_SUBFOLDER') . '/rates', array('controller' => 'RateController', 'method'=>'index'), array()));
 $routes->add('product', new Route(constant('URL_SUBFOLDER') . '/product/{id}', array('controller' => 'ProductController', 'method'=>'showAction'), array('id' => '[0-9]+')));
