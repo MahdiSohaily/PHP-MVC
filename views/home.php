1e2993c34df18a9b70bf87a999e3adb2a995f819
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-04-16 09:19:00 +0430

    Add style fiels

--- views/home.php
+++ views/home.php
@@ -5,11 +5,11 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="This is a simple CMS for tracing goods based on thier serail or part number.">
+    <meta name="author" content="Mahdi Rezaei">
     <link rel="shortcut icon" href="favicon.png">
-    <title>Simple PHP MVC</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Yadak Shop</title>
+    <link rel="stylesheet" href="./public/css/styles.css">
 </head>
 
 <body>
@@ -24,7 +24,6 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" 
 		integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" 
 		crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 
 </html>
