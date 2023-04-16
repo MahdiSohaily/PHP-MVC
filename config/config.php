a4eea7b1c5f4fd2ca4758b537fb224e3ed49a5bd
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-04-16 08:59:43 +0430

    Add project confiuratio files

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+//site name
+define('SITE_NAME', 'your-site-name');
+
+//App Root
+define('APP_ROOT', dirname(dirname(__FILE__)));
+define('URL_ROOT', '/');
+define('URL_SUBFOLDER', 'yadak');
+
+//DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'yadak');
