1c55ef8ab0d589909fe7e5d63c74ab02aa61f0f7
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-04-17 15:50:11 +0430

    Add goods list route

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,45 @@ use Symfony\Component\Routing\RouteCollection;
 
 // Routes system
 $routes = new RouteCollection();
-$routes->add('login', new Route(constant('URL_SUBFOLDER') . '/', array('controller' => 'LoginController', 'method'=>'index'), array()));
-$routes->add('homepage', new Route(constant('URL_SUBFOLDER') . '/search', array('controller' => 'PageController', 'method'=>'index'), array()));
-$routes->add('goods', new Route(constant('URL_SUBFOLDER') . '/goods', array('controller' => 'GoodController', 'method'=>'index'), array()));
-$routes->add('rates', new Route(constant('URL_SUBFOLDER') . '/rates', array('controller' => 'RateController', 'method'=>'index'), array()));
-$routes->add('product', new Route(constant('URL_SUBFOLDER') . '/product/{id}', array('controller' => 'ProductController', 'method'=>'showAction'), array('id' => '[0-9]+')));
+$routes->add('login', 
+    new Route(constant('URL_SUBFOLDER') . '/', 
+    array('controller' => 'LoginController', 
+    'method'=>'index'), array())
+);
 
-$routes->add('getdata', new Route(constant('URL_SUBFOLDER') . '/getdata/{key}/{mode}', array('controller' => 'SearchController', 'method'=>'index'), array('key' => '[a-zA-Z0-9]+','mode'=>'[0-1]')));
+$routes->add('homepage', 
+    new Route(constant('URL_SUBFOLDER') . '/search', 
+    array('controller' => 'PageController', 
+    'method'=>'index'), array())
+);
+
+$routes->add('goods',
+    new Route(constant('URL_SUBFOLDER') . '/goods',
+    array('controller' => 'GoodController',
+    'method'=>'index'), array())
+);
+
+$routes->add('goodslist',
+    new Route(constant('URL_SUBFOLDER') . '/goodslist',
+    array('controller' => 'GoodController',
+    'method'=>'list'), array())
+);
+
+
+$routes->add('rates',
+    new Route(constant('URL_SUBFOLDER') . '/rates',
+    array('controller' => 'RateController',
+    'method'=>'index'), array())
+);
+
+$routes->add('product',
+    new Route(constant('URL_SUBFOLDER') . '/product/{id}',
+    array('controller' => 'ProductController',
+    'method'=>'showAction'), array('id' => '[0-9]+'))
+);
+
+$routes->add('getdata',
+    new Route(constant('URL_SUBFOLDER') .'/getdata/{key}/{mode}',
+    array('controller' => 'SearchController', 'method'=>'index'),
+    array('key' => '[a-zA-Z0-9]+','mode'=>'[0-1]'))
+);
