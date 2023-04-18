7a6c353fb26210ddddac843d6398fec2a5abb5e9
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-04-18 19:18:58 +0430

    Update nav routes

--- views/home.php
+++ views/home.php
@@ -25,31 +25,31 @@
         <nav>
             <ul class="nav">
                 <li class="nav-link">
-                    <a href="">
+                    <a href="<?php echo URL_ROOT.URL_SUBFOLDER ?>">
                         <i class="material-icons">search</i>
                         <span>Search</span>
                     </a>
                 </li>
                 <li class="nav-link">
-                    <a href="goods">
+                    <a href="<?php echo URL_ROOT.URL_SUBFOLDER ?>/goods">
                         <i class="material-icons">book</i>
                         <span>Register Goods</span>
                     </a>
                 </li>
                 <li class="nav-link">
-                    <a href="goodslist">
+                    <a href="<?php echo URL_ROOT.URL_SUBFOLDER ?>/goodslist">
                         <i class="material-icons">format_list_bulleted</i>
                         <span>Goods List</span>
                     </a>
                 </li>
                 <li class="nav-link">
-                    <a href="rates">
+                    <a href="<?php echo URL_ROOT.URL_SUBFOLDER ?>/rates">
                         <i class="material-icons">monetization_on</i>
                         <span>Register Rates</span>
                     </a>
                 </li>
                 <li class="nav-link">
-                    <a href="rateslist">
+                    <a href="<?php echo URL_ROOT.URL_SUBFOLDER ?>/rateslist">
                         <i class="material-icons">filter_list</i>
                         <span>Rates List</span>
                     </a>
@@ -126,29 +126,28 @@
         });
 
         function search(val) {
-        let pattern = val;
-        let supermode = 0;
-        const resultBox = document.getElementById('s-result')
+            let pattern = val;
+            let supermode = 0;
+            const resultBox = document.getElementById('s-result')
 
 
-        if (document.getElementById('mode').checked) {
-            supermode = 1;
+            if (document.getElementById('mode').checked) {
+                supermode = 1;
+            }
+
+            if (pattern.length > 4) {
+                resultBox.innerHTML = "";
+                axios.get('getdata/' + pattern + '/' + supermode)
+                    .then(response => {
+                        resultBox.innerHTML = response.data;
+                    }).catch(error => {
+                        console.log(error);
+                    })
+            } else {
+                resultBox.innerHTML = "";
+            }
         }
-
-        if (pattern.length > 4) {
-            resultBox.innerHTML = "";
-            axios.get('getdata/' + pattern + '/' + supermode)
-                .then(response => {
-                    resultBox.innerHTML = response.data;
-                }).catch(error => {
-                    console.log(error);
-                })
-        } else {
-            resultBox.innerHTML = "";
-        }
-    }
     });
-
     </script>
 </body>
 
