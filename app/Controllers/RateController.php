b22c3b168545d1309c014b7c297e1f3affac0b27
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-04-19 09:35:49 +0430

    Add rate edit controller's function

--- app/Controllers/RateController.php
+++ app/Controllers/RateController.php
@@ -59,4 +59,33 @@ class RateController
 		}
 	}
 
+	public function edit($id, RouteCollection $routes)
+	{
+		if(isset($_COOKIE['login-user'])) {
+			$good = new Good();
+			$edit = $good->find($id);
+			$message = null;
+
+			if(isset($_POST['submit'])){ // Check if form was submitted
+				$id = $_POST['id'];
+				$price = $_POST['price'];
+				$weight= $_POST['weight'];
+				$mobis= $_POST['mobis'];
+
+				$edit = $good->update($id, $price, $weight, $mobis);
+
+				if($edit) {
+					$message = 'Data edited successfuly';
+				} else {
+					$message = 'An Error occurred while saving data';
+				}
+			}
+
+			require_once APP_ROOT . '/views/Good/edit.php';
+		} else {
+			header('Location: /yadak');
+			exit;
+		}
+	}
+
 }
