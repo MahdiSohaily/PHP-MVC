525090ec5e8e3b3812dd10e3a72073b298418108
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-04-24 10:41:48 +0430

    Update database name

--- app/Models/Good.php
+++ app/Models/Good.php
@@ -15,7 +15,7 @@ class Good
 		$servername = "localhost";
         $username = "yadakcenter2";
         $password = "vZun$2*04Bo]";
-        $dbname = "yadakinfo_price";
+        $dbname = "yadakcenter2_yadakinfo_price";
 
         // Create connection
         $conn = mysqli_connect($servername, $username, $password,$dbname);
@@ -41,7 +41,7 @@ class Good
         $servername = "localhost";
         $username = "yadakcenter2";
         $password = "vZun$2*04Bo]";
-        $dbname = "yadakinfo_price";
+        $dbname = "yadakcenter2_yadakinfo_price";
 
         // Create connection
         $conn = mysqli_connect($servername, $username, $password,$dbname);
@@ -137,7 +137,7 @@ class Good
         $servername = "localhost";
         $username = "yadakcenter2";
         $password = "vZun$2*04Bo]";
-        $dbname = "yadakinfo_price";
+        $dbname = "yadakcenter2_yadakinfo_price";
 
         // Create connection
         $conn = mysqli_connect($servername, $username, $password,$dbname);
@@ -164,7 +164,7 @@ class Good
         $servername = "localhost";
         $username = "yadakcenter2";
         $password = "vZun$2*04Bo]";
-        $dbname = "yadakinfo_price";
+        $dbname = "yadakcenter2_yadakinfo_price";
 
         // Create connection
         $conn = mysqli_connect($servername, $username, $password,$dbname);
@@ -191,7 +191,7 @@ class Good
         $servername = "localhost";
         $username = "yadakcenter2";
         $password = "vZun$2*04Bo]";
-        $dbname = "yadakinfo_price";
+        $dbname = "yadakcenter2_yadakinfo_price";
 
         // Create connection
         $conn = mysqli_connect($servername, $username, $password,$dbname);
@@ -246,7 +246,7 @@ class Good
         $servername = "localhost";
         $username = "yadakcenter2";
         $password = "vZun$2*04Bo]";
-        $dbname = "yadakinfo_price";
+        $dbname = "yadakcenter2_yadakinfo_price";
 
         // Create connection
         $conn = mysqli_connect($servername, $username, $password,$dbname);
@@ -301,7 +301,7 @@ class Good
         $servername = "localhost";
         $username = "yadakcenter2";
         $password = "vZun$2*04Bo]";
-        $dbname = "yadakinfo_price";
+        $dbname = "yadakcenter2_yadakinfo_price";
 
         // Create connection
         $conn = mysqli_connect($servername, $username, $password,$dbname);
@@ -316,7 +316,7 @@ class Good
         $servername = "localhost";
         $username = "yadakcenter2";
         $password = "vZun$2*04Bo]";
-        $dbname = "yadakinfo_price";
+        $dbname = "yadakcenter2_yadakinfo_price";
 
         // Create connection
         $conn = mysqli_connect($servername, $username, $password,$dbname);
@@ -333,7 +333,7 @@ class Good
 		$servername = "localhost";
         $username = "yadakcenter2";
         $password = "vZun$2*04Bo]";
-        $dbname = "yadakinfo_price";
+        $dbname = "yadakcenter2_yadakinfo_price";
 
         // Create connection
         $conn = mysqli_connect($servername, $username, $password,$dbname);
@@ -352,7 +352,7 @@ class Good
         $servername = "localhost";
         $username = "yadakcenter2";
         $password = "vZun$2*04Bo]";
-        $dbname = "yadakinfo_price";
+        $dbname = "yadakcenter2_yadakinfo_price";
 
         // Create connection
         $conn = mysqli_connect($servername, $username, $password,$dbname);
@@ -411,7 +411,7 @@ class Good
         $servername = "localhost";
         $username = "yadakcenter2";
         $password = "vZun$2*04Bo]";
-        $dbname = "yadakinfo_price";
+        $dbname = "yadakcenter2_yadakinfo_price";
 
         // Create connection
         $conn = mysqli_connect($servername, $username, $password,$dbname);
