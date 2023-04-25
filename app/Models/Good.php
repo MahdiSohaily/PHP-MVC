e7fc54fb01600ef5a607a7c47768b26c679e3877
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-04-25 10:44:17 +0430

    Update mobis html structure

--- app/Models/Good.php
+++ app/Models/Good.php
@@ -434,8 +434,6 @@ class Good
 
         $template = '';
         if ($result->num_rows > 0) {
-    
-            include_once('../../views/simple_html_dom.php');
 
             $context = stream_context_create(array("http" => array("header" => "User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36")));
 
